2c8bc76d7f53ee3b61985a0bcb6fddb03be5678e
kiripcha <70681104+kiripcha@users.noreply.github.com>
2021-11-09 16:35:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
+from multiprocessing import Process,  Event
+import shutil
+from pathlib import Path
+
+import keyboard
+
+import matrix
+
+MATRICES_DIR = 'matrices'
+FILENAME_BASE = 'matrix'
+STOP_KEY = 'x'
 
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+def task(n, min_value, max_value, stop):
+    count = 1
+    while not stop.is_set():
+        first_matrix = matrix.sqr_random(n, min_value, max_value)
+        first_matrix.write(f'{MATRICES_DIR}/{count}_{FILENAME_BASE}_first.csv')
+        second_matrix = matrix.sqr_random(n, min_value, max_value)
+        second_matrix.write(f'{MATRICES_DIR}/{count}_{FILENAME_BASE}_second.csv')
+        result_matrix = first_matrix.multiply(
+            second_matrix, f'{MATRICES_DIR}/{count}_{FILENAME_BASE}_temp.csv'
+        )
+        result_matrix.write(f'{MATRICES_DIR}/{count}_{FILENAME_BASE}_result.csv')
+        count += 1
 
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+def recreate_matrices_dir():
+    shutil.rmtree(MATRICES_DIR)
+    Path(MATRICES_DIR).mkdir()
 
-print(element((1, 0), matrix1, matrix2))
+
+def _main():
+    recreate_matrices_dir()
+    stop = Event()
+    n = int(input('Введите размерность матриц: '))
+    min_value = int(input('Введите минимальный элемент матриц: '))
+    max_value = int(input('Введите максимальный элемент матриц: '))
+    p = Process(target=task, args=(n, min_value, max_value, stop))
+    p.start()
+    while True:
+        if keyboard.is_pressed(STOP_KEY):
+            stop.set()
+            break
+    p.join()
+
+
+if __name__ == '__main__':
+    _main()
